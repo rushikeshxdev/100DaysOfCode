93ce0e4c07ba65331b3037d26f49a6550ce45bf6
imrushikes09 <rushirandive31@gmail.com>
2025-06-20 18:46:22 +0530

    Day-8: Implemented Bubble & Selection Sort in C++, solved 5 LeetCode sorting problems, set up React portfolio project structure

---
+++ Day-8/Sorting/MergeSort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Merge two sorted halves
+void merge(vector<int> &arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    // Merge the two arrays
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
+    }
+
+    // Copy any remaining elements
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+// Recursive function
+void mergeSort(vector<int> &arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        // Sort each half
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    vector<int> arr = {12, 11, 13, 5, 6, 7};
+
+    cout << "Original array: ";
+    for (int num : arr) cout << num << " ";
+    cout << endl;
+
+    mergeSort(arr, 0, arr.size() - 1);
+
+    cout << "Sorted array: ";
+    for (int num : arr) cout << num << " ";
+    cout << endl;
+
+    return 0;
+}
